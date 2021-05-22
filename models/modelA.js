1c569f9415c4f3b59da7ce92138379a61aedd2e0
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2021-05-21 20:54:52 -0400

    Create laptop object

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var laptop = {
+  brand: 'Apple',
+  model: 'Macbook Pro',
+  price: 1200.00,
+  tradeValue: 600.00,
+  isNew: true,
+  isUsed: false,
+  ssdOptions: ['256gb','512gb','1TB'],
+  colorOptions:['silver','space grey'],
+}
