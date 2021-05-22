dfcc8c4567367e128d0d61d2c09506efe0afb3dd
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2021-05-21 21:09:38 -0400

    Create house object

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var house = {
+  street: '123 fake street',
+  city: 'Denver',
+  zipcode: 12345,
+  houseValue: 500000.00,
+  isForSale: true,
+  isOccupied: false,
+  nearestSchools: ['school 1','school 2','school 3'],
+  nearestResturants: ['Red Lobster','Capital Grille','Olive Garden']
+}
